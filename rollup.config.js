41e947f807bfc621a3e5c53e22116c8c771db4c2
Zack McCartney <zack@bigroomstudios.com>
2021-05-04 13:10:59 -0400

    Revise rollup babel configs, add umd demo page

--- rollup.config.js
+++ rollup.config.js
@@ -24,11 +24,16 @@ module.exports = [
                 sourcemap: true
             }
         ],
+        external: [/@babel\/runtime/],
         plugins: [
             PeerDepsExternal(),
             NodeResolve(),
             Commonjs(),
-            Babel({ exclude: ['node_modules/**'], babelHelpers: 'bundled' }),
+            Babel({
+                exclude: ['node_modules/**'],
+                babelHelpers: 'runtime',
+                plugins: ['@babel/plugin-transform-runtime']
+            }),
             Filesize()
         ]
     },
@@ -49,11 +54,23 @@ module.exports = [
                 react: 'React'
             }
         },
+        // core-js polyfills are imported for node/esm compatibility, so core-js@3.11.x is
+        // technically a peer for consuming the UMD output those environments e.g. node, but bundled
+        // for running in-browser
+        external: [/^core\-js/],
         plugins: [
             PeerDepsExternal(),
             NodeResolve(),
             Commonjs(),
-            Babel({ exclude: ['node_modules/**'], babelHelpers: 'bundled' }),
+            Babel({
+                exclude: ['node_modules/**'],
+                // babelHelpers and useBuiltIns together result in our used polyfills e.g.
+                // regenerator-runtime bundled in the output UMD, so it's directly usable
+                // in-browser (no need to pull in anything from core-js via preceeding
+                // script tags)
+                babelHelpers: 'bundled',
+                presets: [['@babel/env', { useBuiltIns: 'usage', corejs: '3.11' }]]
+            }),
             Terser(),
             Filesize()
         ]
